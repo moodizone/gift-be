c040ac23418de46c6c8cebd3421a769944e9baa9
moodi <moodizone@gmail.com>
2024-11-10 21:22:12 +0330

    🥅 add error handler and wildcard 404

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 import path from "path";
 import logger from "morgan";
 import cookieParser from "cookie-parser";
+import httpErrors from "http-errors";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
+const port = 3002;
 const app = express();
 
 // logger for all http requests in dev
@@ -21,8 +23,21 @@ app.use("/assets", express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-app.listen(3002, () => {
-  console.log(`App is running at port ${3002}`);
+// wild card routes
+app.use(function (_req, _res, next) {
+  next(httpErrors.NotFound());
+});
+
+// error handler
+app.use(((err, _req, res, _next) => {
+  console.error(err.stack);
+  // render the error page
+  res.status(err.status || 500);
+  res.render("Something went wrong, That's all we know");
+}) as ErrorRequestHandler);
+
+app.listen(port, () => {
+  console.log(`App is running at port ${port}`);
 });
 
 export default app;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 const router = express.Router();
 router.get("/", function (req, res, next) {
+  throw new Error("BROKEN");
   res.send("respond with a resource");
 });
 
