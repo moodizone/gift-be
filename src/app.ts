ee068d6620696d8e70263ed314eb05407345d14a
moodi <moodizone@gmail.com>
2024-11-10 12:22:23 +0330

    🔊 add logger on dev

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
+import logger from "morgan";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
 const app = express();
+
+// logger for all http requests in dev
+app.use(logger("dev"));
 
 // virtual `/assets` path
 app.use("/assets", express.static(path.join(__dirname, "public")));
