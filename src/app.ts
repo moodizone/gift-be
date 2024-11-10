f7ef7802184e2a3eff9ac83a0c412d87dc291c7e
moodi <moodizone@gmail.com>
2024-11-10 12:46:28 +0330

    📦 add cookie parser to access cookies in request object

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import logger from "morgan";
+import cookieParser from "cookie-parser";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -9,6 +10,9 @@ const app = express();
 
 // logger for all http requests in dev
 app.use(logger("dev"));
+
+// extract cookies from headers and populate to request object
+app.use(cookieParser());
 
 // virtual `/assets` path
 app.use("/assets", express.static(path.join(__dirname, "public")));
