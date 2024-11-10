2e3ff17ca5230cf8c9213b5b82ff6b90107d7c07
moodi <moodizone@gmail.com>
2024-11-10 11:21:13 +0330

    🚧 test virtual asset segment

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser(),2);
+
+// virtual `/assets` path
+app.use("/assets", express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
