741b97128e5cca5331d201e1090eba206263b451
moodi <moodizone@gmail.com>
2024-11-13 20:20:38 +0330

    👽️ check api to database

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { pool } from "../db";
 
 const router = express.Router();
-router.get("/", function (req, res, next) {
-  throw new Error("BROKEN");
-  res.send("respond with a resource");
+router.get("/", async function (req, res, next) {
+  try {
+    await pool.query('INSERT INTO "User" (email, tel) VALUES ($1, $2)', [
+      "test_email@express.com",
+      "12344321",
+    ]);
+    res.send("successful");
+  } catch (error) {
+    console.log(error);
+    res.send("failed");
+  }
 });
 
 export default router;
