37a959f93fdb4548ba08f67c2711b6ac9c96233f
moodi <moodizone@gmail.com>
2024-11-15 18:53:52 +0330

    🚧 exclude password on result

--- src/models/users.ts
+++ src/models/users.ts
@@ -13,7 +13,8 @@ export async function createUserQuery({
   password,
 }: CreateUserType) {
   const result = await pool.query<UserType>(
-    `INSERT INTO public."User" (tel, name, email, gender, password) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    `INSERT INTO public."User" (tel, name, email, gender, password) VALUES ($1, $2, $3, $4, $5) 
+    RETURNING id, tel, name, email, gender`,
     [tel, name, email, gender, password]
   );
   return result.rows[0];
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { getUsers } from "../controllers/users";
+import { createUser, getUsers } from "../controllers/users";
 import { asyncHandler } from "../middlewares/asyncHandler";
 import { validateRequest } from "../middlewares/validate";
 import { createUserSchema } from "../validation/user";
 
 const router = express.Router();
 router.get("/", asyncHandler(getUsers));
-router.post("/", validateRequest(createUserSchema), asyncHandler(getUsers));
+router.post("/", validateRequest(createUserSchema), asyncHandler(createUser));
 
 export default router;
